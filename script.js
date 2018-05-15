df7b0ee1c828e97b92b94dc1e8bf33009cf540a7
emachaffie <emachaffie@gmail.com>
2018-05-15 13:59:38 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+import lizdata from 'superagent'
+
+// function onLoad to display _______ 
+function onLoad (event) {
+  event.preventDefault()
+  console.log('loaded')
+  // more
+}
+
+// function to request data from API
+function requestData (lizdata) {
+  request.get(`https://api.github.com/users/emachaffie?=${lizdata}`)
+    .then(function (result) {
+
+    })
+}
+
+// functions for each section to make variables
+// const fullname = requestData()
+
+// function to put data from API into each section
+
+// function to output section into appropriate place in HTML
+
+window.addEventListener('load', onLoad)
+
+// function onSubmit (event) {
+//   event.preventDefault()
+//   console.log('submitted!')
+//   const query = document.getElementById('query').value
+//   requestData(query)
+// }
+
+// function requestData (query) {
+//   if (query === '') { return }
+//   console.log('query', query)
+//   request.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+//     .then(function (result) {
+//       console.log('result', result)
+//       displayResults(result.body.items)
+//     })
+// }
+
+// function displayResults (books) {
+//   const booksAsHTML = books.map(book => createBookHTML(book))
+//   document.getElementById('results').innerHTML = booksAsHTML.join('')
+// }
+
+// function createBookHTML (book) {
+//   return `
+//   <div class="book row">
+//     <div class="col">
+//       <h2>${book.volumeInfo.title}</h2>
+//       <dl>
+//         <dt>Author</dt>
+//         <dd>${book.volumeInfo.authors.join(', ')}</dd>
+//         <dt>Publisher</dt>
+//         <dd>${book.volumeInfo.publisher}</dd>
+//       </dl>
+//     </div>
+//     <div class="col">
+//       <img src="${book.volumeInfo.imageLinks.smallThumbnail}">
+//     </div>
+//   </div>
+//   <hr />
+//   `
+// }
